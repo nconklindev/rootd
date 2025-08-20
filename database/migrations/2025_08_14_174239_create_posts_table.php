eb99d501e240b3da6e092b3c09e7957d43bc9fa2
nconklindev <190518646+nconklindev@users.noreply.github.com>
2025-08-20 16:55:33 -0400

    feat: update posts table migration
    Added new columns `title`, `body`, and `views_count` to the `posts` table. Marked the `body` column as nullable and provided a default value of 0 for `views_count`.

--- database/migrations/2025_08_14_174239_create_posts_table.php
+++ database/migrations/2025_08_14_174239_create_posts_table.php
@@ -5,17 +5,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
             $table->string('slug');
             $table->text('content');
+            $table->text('body')->nullable();
             $table->string('excerpt');
             $table->string('type');
-            $table->string('status');
+            $table->unsignedBigInteger('views_count')->default(0);
             $table->foreignIdFor(User::class)->constrained('users');
             $table->timestamps();
         });
