ba29ff829d34b55d7c077a1442826f12463de026
nconklindev <190518646+nconklindev@users.noreply.github.com>
2025-08-20 18:22:38 -0400

    refactor(middleware): simplify `share()` method with `array_merge`
    Replaced spread operator with `array_merge` for improved clarity and consistency in the `share` method.
    
    Adjusted formatting to enhance code readability and maintainability.
    
    See https://inertiajs.com/shared-data

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,18 +39,18 @@ class HandleInertiaRequests extends Middleware
     {
         [$message, $author] = str(Inspiring::quotes()->random())->explode('-');
 
-        return [
-            ...parent::share($request),
-            'name' => config('app.name'),
-            'quote' => ['message' => trim($message), 'author' => trim($author)],
-            'auth' => [
-                'user' => $request->user(),
-            ],
-            'ziggy' => [
-                ...(new Ziggy)->toArray(),
-                'location' => $request->url(),
-            ],
-            'sidebarOpen' => ! $request->hasCookie('sidebar_state') || $request->cookie('sidebar_state') === 'true',
-        ];
+        return
+            array_merge(parent::share($request), [
+                'name' => config('app.name'),
+                'quote' => ['message' => trim($message), 'author' => trim($author)],
+                'auth' => [
+                    'user' => $request->user(),
+                ],
+                'ziggy' => [
+                    ...(new Ziggy)->toArray(),
+                    'location' => $request->url(),
+                ],
+                'sidebarOpen' => !$request->hasCookie('sidebar_state') || $request->cookie('sidebar_state') === 'true',
+            ]);
     }
 }
