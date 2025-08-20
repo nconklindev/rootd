1fd722fee8886ccdf9243a84e96f0612e9c917a3
nconklindev <190518646+nconklindev@users.noreply.github.com>
2025-08-20 17:07:19 -0400

    feat(models): add polymorphic relation to Like model
    Introduced a `likeable()` method in the `Like` model to define a polymorphic relationship.
    
    This allows the `Like` model to associate with multiple types of models, improving flexibility and reusability.

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Like extends Model
 {
@@ -17,5 +18,10 @@ class Like extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function likeable(): MorphTo
+    {
+        return $this->morphTo();
     }
 }
