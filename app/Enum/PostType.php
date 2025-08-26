4aabbe84136ce8d8e4f8341834932a3c4f4a453b
nconklindev <190518646+nconklindev@users.noreply.github.com>
2025-08-25 20:11:39 -0400

    feat(posts): refactor PostType enum with simplified cases
    Replaced specific post type cases (`Code`, `File`, `Image`, `Article`, `Link`) with generalized cases (`Text`, `Media`). Updated `icon` and `label` methods to reflect new case mappings for improved maintainability and code clarity.

--- app/Enum/PostType.php
+++ app/Enum/PostType.php
@@ -6,37 +6,25 @@ use App\Contracts\Iconable;
 
 enum PostType: string implements Iconable
 {
-    case Code = 'code';
-    case File = 'file';
-    case Image = 'image';
-    case Article = 'article';
-    case Link = 'link';
+    case Text = 'text';
+    case Media = 'media';
 
     /**
      * Defines the lucide-vue-next icon names to be used for each PostTYpe
-     *
-     * @return string
      */
     public function icon(): string
     {
         return match ($this) {
-            self::Code => 'Code2',
-            self::File => 'FileTerminal',
-            self::Image => 'Image',
-            self::Article => 'FileText',
-            self::Link => 'Link2'
+            self::Text => 'FileText',
+            self::Media => 'Image',
         };
     }
 
     public function label(): string
     {
         return match ($this) {
-            self::Code => 'Code',
-            self::File => 'File',
-            self::Image => 'Image',
-            self::Article => 'Article',
-            self::Link => 'Link'
+            self::Text => 'Text',
+            self::Media => 'Media',
         };
     }
 }
-
