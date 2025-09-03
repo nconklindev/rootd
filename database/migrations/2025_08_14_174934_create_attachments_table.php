d7decd5ec1f6f6408504d8a42301eee5cbd48573
nconklindev <190518646+nconklindev@users.noreply.github.com>
2025-09-03 19:47:20 -0400

    feat(migrations): enhance attachments table structure
    Added `cascadeOnDelete` to foreign keys in the attachments table for automatic cleanup of related data. Introduced new `file_path` and `original_filename` fields to store file details and updated `download_count` with a default value of 0.

--- database/migrations/2025_08_14_174934_create_attachments_table.php
+++ database/migrations/2025_08_14_174934_create_attachments_table.php
@@ -6,18 +6,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('attachments', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Post::class)->constrained('posts');
-            $table->foreignIdFor(User::class)->constrained('users');
+            $table->foreignIdFor(Post::class)->constrained('posts')->cascadeOnDelete();
+            $table->foreignIdFor(User::class)->constrained('users')->cascadeOnDelete();
             $table->unsignedBigInteger('file_size');
+            $table->string('file_path');
+            $table->string('original_filename');
             $table->string('mime_type');
-            $table->string('type');
-            $table->unsignedBigInteger('download_count');
+            $table->unsignedBigInteger('download_count')->default(0);
             $table->timestamps();
         });
     }
