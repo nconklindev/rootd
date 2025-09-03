c8f5b28c5ba6323a00de18574f5e1cf78c4a7b93
nconklindev <190518646+nconklindev@users.noreply.github.com>
2025-09-03 19:48:45 -0400

    feat(requests): update validation rules for StorePostRequest
    Added validation rules for a new `file` field, including conditional requirement for `media` type posts, file size limit (15MB), and supported file mime types. Introduced a `link` field to replace removed `excerpt` and `body` fields, ensuring better data handling and alignment with current feature set.
    
    Removed outdated `body` and `excerpt` validation rules, reflecting recent changes to post creation requirements.

--- app/Http/Requests/StorePostRequest.php
+++ app/Http/Requests/StorePostRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests;
 use App\Enum\PostType;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\File;
 
 class StorePostRequest extends FormRequest
 {
@@ -18,12 +19,18 @@ class StorePostRequest extends FormRequest
         return [
             'title' => ['required', 'string', 'max:255'],
             'content' => ['required', 'string'],
-            'body' => ['nullable', 'string'],
-            'excerpt' => ['nullable', 'string', 'max:255'],
+            'link' => ['nullable', 'url', 'max:255'],
             'type' => ['nullable', Rule::enum(PostType::class)],
             'category_id' => ['nullable', 'exists:categories,id'],
             'tags' => ['nullable', 'array', 'max:5'],
             'tags.*' => ['string', 'max:25'],
+            'file' => [
+                'nullable',
+                'required_if:type,media',
+                'file',
+                'max:15360', // 15MB in kilobytes
+                'mimes:jpg,jpeg,png,gif,webp,pdf,doc,docx,txt,py,bat,sh,rb,php,js,ts,json,zip,rar,7z,tif,rs,cs,c,go,lua,sql,perl,java,kotlin,asm,xml,ps,ps1'
+            ]
         ];
     }
 }
