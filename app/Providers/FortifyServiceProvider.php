0fe01e500ff845aaba8e183267df360df8877ba9
nconklindev <190518646+nconklindev@users.noreply.github.com>
2025-09-17 18:39:36 -0400

    feat(fortify): implement FortifyServiceProvider for two-factor auth
    Added `FortifyServiceProvider` to handle views for login, two-factor challenge, and password confirmation. Configured rate limiting for login and two-factor endpoints. Registered actions for updating user profile information and passwords.
    
    Refs feature/two-factor-authentication

---
+++ app/Providers/FortifyServiceProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Providers;
+
+use App\Actions\Fortify\CreateNewUser;
+use App\Actions\Fortify\ResetUserPassword;
+use App\Actions\Fortify\UpdateUserPassword;
+use App\Actions\Fortify\UpdateUserProfileInformation;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+use Inertia\Inertia;
+use Laravel\Fortify\Actions\RedirectIfTwoFactorAuthenticatable;
+use Laravel\Fortify\Fortify;
+
+class FortifyServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        // We aren't using the Fortify Features that would warrant including these in here
+//        Fortify::createUsersUsing(CreateNewUser::class);
+        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
+        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
+//        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+        Fortify::loginView(function () {
+            return Inertia::render('auth/Login');
+        });
+        Fortify::twoFactorChallengeView(function () {
+            return Inertia::render('auth/TwoFactorChallenge');
+        });
+        Fortify::confirmPasswordView(function () {
+            return Inertia::render('auth/ConfirmPassword');
+        });
+        Fortify::redirectUserForTwoFactorAuthenticationUsing(RedirectIfTwoFactorAuthenticatable::class);
+        
+        RateLimiter::for('login', function (Request $request) {
+            $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())) . '|' . $request->ip());
+
+            return Limit::perMinute(5)->by($throttleKey);
+        });
+
+        RateLimiter::for('two-factor', function (Request $request) {
+            return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        });
+    }
+}
