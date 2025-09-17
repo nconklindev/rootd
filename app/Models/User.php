10791509fbae551834942df9971e138d1ad1ee07
nconklindev <190518646+nconklindev@users.noreply.github.com>
2025-09-17 18:38:35 -0400

    feat(models): add two-factor authentication to User model
    Integrated Laravel Fortify's `TwoFactorAuthenticatable` trait into the `User` model, enabling two-factor authentication for added security. Updated the model to include necessary imports and trait usage.
    
    This change ensures the user accounts are compatible with two-factor authentication features introduced via Fortify.
    
    Refs feature/two-factor-authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,12 +9,13 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, HasRoles, Notifiable;
+    use HasFactory, HasRoles, Notifiable, TwoFactorAuthenticatable;
 
     protected $fillable = [
         'name',
